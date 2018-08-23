7bed8e759fce4deb8f7d34bf45561d9f931a2892
CeShine Lee <shuanck@gmail.com>
2018-08-23 15:48:36 +0800

    Word segmentation for rating dataset

---
+++ scripts/segment_ratings.py
@@ -0,0 +1,39 @@
+import subprocess
+import re
+
+import pandas as pd
+from tqdm import tqdm
+
+DATAPATH = "data/ratings.csv"
+TMPPATH = "/tmp/ratings.txt"
+TMPPATH_WORD = "/tmp/ratings_words.txt"
+TARGETPATH = "data/ratings_words.csv"
+
+
+def main():
+    ratings = pd.read_csv(DATAPATH)
+    with open(TMPPATH, "w") as fw:
+        for row in ratings["comment"]:
+            fw.write(row + "\n")
+    # Tokenization
+    res = subprocess.run([
+        "thulac", "-model_dir", "/mnt/SSD_Data/openai_nlp/THULAC/models/",
+        "-seg_only", "-input", TMPPATH, "-output", TMPPATH_WORD
+    ], stdout=subprocess.PIPE)
+    print(res)
+
+    comments = []
+    with open(TMPPATH_WORD) as f:
+        for line in f.readlines():
+            line = re.sub(r"\s+", " ", line).strip()
+            comments.append(line)
+
+    final_ratings = pd.DataFrame({
+        "comments": comments,
+        "rating": ratings.rating
+    })
+    final_ratings.to_csv(TARGETPATH, index=False)
+
+
+if __name__ == "__main__":
+    main()
